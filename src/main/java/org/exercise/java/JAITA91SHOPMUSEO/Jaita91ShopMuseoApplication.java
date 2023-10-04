c312b10a617c40100b86ae256726b3b6cda991b9
Burdusa Daniel <danielburdusa26@gmail.com>
2023-10-04 11:04:00 +0200

    Initial commit

---
+++ src/main/java/org/exercise/java/JAITA91SHOPMUSEO/Jaita91ShopMuseoApplication.java
@@ -0,0 +1,13 @@
+package org.exercise.java.JAITA91SHOPMUSEO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jaita91ShopMuseoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jaita91ShopMuseoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/exercise/java/JAITA91SHOPMUSEO/Jaita91ShopMuseoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.exercise.java.JAITA91SHOPMUSEO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jaita91ShopMuseoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
