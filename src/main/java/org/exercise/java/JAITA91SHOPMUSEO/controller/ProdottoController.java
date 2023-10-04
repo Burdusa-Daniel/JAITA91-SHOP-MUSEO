5f5990d34b1809e3d3d711c3d4ce46acebdb7e2d
Sanchez <bradsanchez9845@gmail.com>
2023-10-04 15:42:34 +0200

    controller admin : homepage e create

--- src/main/java/org/exercise/java/JAITA91SHOPMUSEO/controller/ProdottoController.java
+++ src/main/java/org/exercise/java/JAITA91SHOPMUSEO/controller/ProdottoController.java
@@ -6,7 +6,10 @@ import org.exercise.java.JAITA91SHOPMUSEO.repository.ProdottoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -19,12 +22,37 @@ public class ProdottoController {
     private ProdottoRepository prodottoRepository;
 
 
-    @GetMapping
+    @GetMapping("/")
     public String index(Model model) {
         List<Prodotto> prodottoList = prodottoRepository.findAll();
         model.addAttribute("prodotti", prodottoList);
         return "prodotti/index";
     }
+
+    @GetMapping("/admin")
+    public String homepage(Model model) {
+        List<Prodotto> prodottoList = prodottoRepository.findAll();
+        model.addAttribute("prodotti", prodottoList);
+        return "admin/homepage";
+    }
+
+    @GetMapping("/admin/create")
+    public String create(Model model){
+        model.addAttribute("prodotti", new Prodotto()){
+            return "admin/create";
+        }
+    }
+
+    @PostMapping("/admin/create")
+    public String doCreate(@ModelAttribute("prodotti") Prodotto prodottoForm,
+    BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return "admin/create";
+        }
+        prodottoRepository.save(prodottoForm);
+        return "redirect:/admin/";
+    }
+
 
 
 }
