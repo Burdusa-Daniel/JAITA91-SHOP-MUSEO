c13c88cfd00a8d0d094fbe26737cad21949ae9d0
Giacomotestori <giacomo.testori10@gmail.com>
2023-10-04 16:29:20 +0200

    created mapping from admin homepage to create html page

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,2 @@
-INSERT INTO prodotti (description, name, price, url) VALUES('dfdfddf', 'louvre', 10.89, 'guardaqua');
+INSERT INTO prodotti (description, name, price, url) VALUES('tazza da te', 'tazza', 7.99, 'https://cdn.pixabay.com/photo/2014/09/24/17/13/mugs-459324_1280.jpg');
+INSERT INTO prodotti (description, name, price, url) VALUES('piatto prima portata', 'piatto', 7.99, 'https://cdn.pixabay.com/photo/2016/12/23/07/49/table-setting-1926936_1280.jpg');
