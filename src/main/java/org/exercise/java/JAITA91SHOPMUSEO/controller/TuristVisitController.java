99a0a907073f0a85dff8b7c325bfa1428a3b2fae
Jean <jmlrissac@gmail.com>
2023-10-12 13:42:50 +0200

    Fixed edit for visits

--- src/main/java/org/exercise/java/JAITA91SHOPMUSEO/controller/TuristVisitController.java
+++ src/main/java/org/exercise/java/JAITA91SHOPMUSEO/controller/TuristVisitController.java
@@ -3,6 +3,7 @@ package org.exercise.java.JAITA91SHOPMUSEO.controller;
 import jakarta.validation.Valid;
 import org.exercise.java.JAITA91SHOPMUSEO.model.TuristVisit;
 import org.exercise.java.JAITA91SHOPMUSEO.repository.TuristVisitRepository;
+import org.exercise.java.JAITA91SHOPMUSEO.service.TuristVisitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,8 +14,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/visita-guidata")
 public class TuristVisitController {
 
+    private final TuristVisitService turistVisitService;
     @Autowired
     private TuristVisitRepository turistVisitRepository;
+
+    public TuristVisitController(TuristVisitService turistVisitService) {
+        this.turistVisitService = turistVisitService;
+    }
 
     @GetMapping
     public String index(Model model) {
@@ -24,33 +30,33 @@ public class TuristVisitController {
 
     @GetMapping("admin/list")
     public String indexAdmin(Model model) {
-        model.addAttribute("turistvisit", turistVisitRepository.findAll());
+        model.addAttribute("turistVisit", turistVisitRepository.findAll());
         return "admin/visit/index";
     }
 
 
     @GetMapping("/edit/{id}")
     public String editVisit(Model model, @PathVariable Integer id) {
-        model.addAttribute("turistvisit", turistVisitRepository.findById(id));
+        model.addAttribute("turistvisit", turistVisitService.getById(id));
         return "admin/visit/edit";
     }
 
-    @PostMapping("/admin/edit")
+    @PostMapping("/admin/edit/{id}")
     public String doEdit(Model model, @PathVariable Integer id, @Valid @ModelAttribute("turistVisit")
     TuristVisit turistVisitForm) {
         turistVisitRepository.save(turistVisitForm);
-        return "redirect: /admin/index";
+        return "redirect:/admin";
     }
 
     @GetMapping("/detail/{id}")
     public String detailVisit(Model model, @PathVariable Integer id) {
-        model.addAttribute("turistvisit", turistVisitRepository.findById(id));
+        model.addAttribute("turistvisit", turistVisitService.getById(id));
         return "visit/detail";
     }
 
     @GetMapping("/admin/detail/{id}")
     public String adminDeatilVisit(Model model, @PathVariable Integer id) {
-        model.addAttribute("turistvisit", turistVisitRepository.findById(id));
+        model.addAttribute("turistvisit", turistVisitService.getById(id));
         return "admin/visit/detail";
     }
 
